87de1fabde33c8a7789a4c3abb35cf58bca24f62
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-05-03 01:38:03 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 let sequelize;
 
-sequelize = new Sequelize(
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -12,6 +15,7 @@ sequelize = new Sequelize(
       dialect: 'mysql',
       port: 3306
     }
-);
+  );
+}
 
 module.exports = sequelize;
