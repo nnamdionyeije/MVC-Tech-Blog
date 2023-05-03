68439e201d9f8abe996efc4a46653e87ce1bafaa
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-05-03 02:12:36 -0400

    hopefully this works

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 module.exports = {
     format_date: (date) => {
       // Format date as MM/DD/YYYY
-      return date.toLocaleDateString([en-US]);
+      return date.toLocaleDateString();
     }
     // format_amount: (amount) => {
     //   // format large numbers with commas
